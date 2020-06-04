44bb2ea484fd5269813fac240bd91070eb9e1df0
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-04 14:10:58 +0430

    create user type

---
+++ schema/schema.js
@@ -0,0 +1,25 @@
+const GraphQL = require('graphql');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
+} = GraphQL;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        lastName: {
+            type: GraphQLString
+        },
+        age: {
+            type: GraphQLInt
+        }
+    }
+});
